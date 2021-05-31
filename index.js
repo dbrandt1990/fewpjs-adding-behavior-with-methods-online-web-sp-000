90595bc3450514b346d744f98271e2c4c83b30f6
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-31 14:51:06 -0600

    commit tree 2d61a06efd92afd580146717c3c41e5c4a0c16f8

--- index.js
+++ index.js
@@ -3,14 +3,27 @@
 class Cat {
     constructor(name) {
         this.name = name;
-    },
-    speak() {
-        `${this.name} says meow!`
+        this.speak = () => `${this.name} says meow!`
     }
 }
 
 class Dog {
+    constructor(name) {
+        this.name = name;
+        this.speak = () => `${this.name} says woof!`
+    }
 }
 
 class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex
+        this.speak = () => {
+            if (this.sex === "male") {
+                return `It's me! ${this.name}, the parrot!`
+            } else {
+                return `${this.name} says squawk!`
+            }
+        };
+    }
 }
